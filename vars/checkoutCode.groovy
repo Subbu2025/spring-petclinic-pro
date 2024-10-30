bb730ad3eb6d2f10ce22bfcb65df0eb19a59ad2b
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-10-31 00:55:47 +0530

    Update checkoutCode.groovy

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,9 +1,16 @@
-def call(Map params) {
+def call(Map params = [:]) {
+    // Validate required parameters
+    assert params.containsKey('credentialsId') : "Missing required parameter: credentialsId"
+    assert params.containsKey('url') : "Missing required parameter: url"
+
+    // Set defaults for optional parameters
     String branch = params.get('branch', 'main')
     String credentialsId = params.get('credentialsId')
-    String repo_url = params.get('url') 
+    String repo_url = params.get('url')
 
     stage('Checkout Code') {
+        echo "Checking out branch '${branch}' from '${repo_url}'"
+        
         checkout([
             $class: 'GitSCM',
             branches: [[name: branch]],
