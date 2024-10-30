578af066882f975b192cc8b107ca2837c11d8c6a
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-10-31 03:36:41 +0530

    Update sonarQubeAnalysis.groovy

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(Map params = [:]) {
-    // Required parameters
     assert params.containsKey('namespace') : "Missing required parameter: namespace"
+    assert params.containsKey('awsCredentialsId') : "Missing required parameter: awsCredentialsId"
 
     String namespace = params.namespace
     String sonarUrl
@@ -8,16 +8,18 @@ def call(Map params = [:]) {
     String sonarToken
 
     stage("SonarQube Analysis") {
-        // Retrieve SonarQube URL and Project Key from the ConfigMap
-        sonarUrl = sh(script: "kubectl get configmap sonar-config -n ${namespace} -o=jsonpath='{.data.SONAR_URL}'", returnStdout: true).trim()
-        sonarProjectKey = sh(script: "kubectl get configmap sonar-config -n ${namespace} -o=jsonpath='{.data.SONAR_PROJECT_KEY}'", returnStdout: true).trim()
+        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: params.awsCredentialsId]]) {
+            // Retrieve SonarQube URL and Project Key from the ConfigMap
+            sonarUrl = sh(script: "kubectl get configmap sonar-config -n ${namespace} -o=jsonpath='{.data.SONAR_URL}'", returnStdout: true).trim()
+            sonarProjectKey = sh(script: "kubectl get configmap sonar-config -n ${namespace} -o=jsonpath='{.data.SONAR_PROJECT_KEY}'", returnStdout: true).trim()
 
-        // Retrieve SonarQube token from the Secret
-        sonarToken = sh(script: "kubectl get secret sonar-secrets -n ${namespace} -o=jsonpath='{.data.SONAR_TOKEN}' | base64 --decode", returnStdout: true).trim()
+            // Retrieve SonarQube token from the Secret
+            sonarToken = sh(script: "kubectl get secret sonar-secrets -n ${namespace} -o=jsonpath='{.data.SONAR_TOKEN}' | base64 --decode", returnStdout: true).trim()
 
-        // Run SonarQube analysis
-        withEnv(["SONAR_HOST_URL=${sonarUrl}", "SONAR_PROJECT_KEY=${sonarProjectKey}", "SONAR_TOKEN=${sonarToken}"]) {
-            sh "./mvnw sonar:sonar -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.projectKey=${SONAR_PROJECT_KEY} -Dsonar.login=${SONAR_TOKEN}"
+            // Run SonarQube analysis
+            withEnv(["SONAR_HOST_URL=${sonarUrl}", "SONAR_PROJECT_KEY=${sonarProjectKey}", "SONAR_TOKEN=${sonarToken}"]) {
+                sh "./mvnw sonar:sonar -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.projectKey=${SONAR_PROJECT_KEY} -Dsonar.login=${SONAR_TOKEN}"
+            }
         }
     }
 }
