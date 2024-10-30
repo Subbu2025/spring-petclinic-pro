8ce8057b534a5054a55bff7ea9aef96ca296458b
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-10-31 00:28:27 +0530

    Update checkoutCode.groovy

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,14 +1,14 @@
-// checkoutCode.groovy
 def call(Map params) {
     String branch = params.get('branch', 'main')
-    String credentialsId = params.get('credentialsId', 'Subbu2025_github-creds')
+    String credentialsId = params.get('credentialsId')
+    String repo_url = params.get('url') 
 
     stage('Checkout Code') {
         checkout([
             $class: 'GitSCM',
             branches: [[name: branch]],
             userRemoteConfigs: [[
-                url: 'https://github.com/Subbu2025/spring-petclinic-pro.git',
+                url: repo_url,
                 credentialsId: credentialsId
             ]]
         ])
