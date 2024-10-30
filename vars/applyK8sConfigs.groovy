563eea181b2c91281538160c500903fab3449745
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-10-31 02:41:14 +0530

    Update applyK8sConfigs.groovy

--- vars/applyK8sConfigs.groovy
+++ vars/applyK8sConfigs.groovy
@@ -1,29 +1,29 @@
 // vars/applyK8sConfigs.groovy
 
 def call(Map params) {
-    // Ensure required parameters
+    assert params.containsKey('repoUrl') : "Missing required parameter: repoUrl"
+    assert params.containsKey('credentialsId') : "Missing required parameter: credentialsId"
     assert params.containsKey('namespace') : "Missing required parameter: namespace"
     assert params.containsKey('environment') : "Missing required parameter: environment"
+    assert params.containsKey('awsCredentialsId') : "Missing required parameter: awsCredentialsId"
 
-    String repoUrl = params.get('repoUrl', '')
-    String credentialsId = params.get('credentialsId', '')
+    String repoUrl = params.repoUrl
+    String credentialsId = params.credentialsId
     String namespace = params.namespace
     String environment = params.environment
+    String awsCredentialsId = params.awsCredentialsId
 
-    // Conditionally check out the repo only if repoUrl and credentialsId are provided
-    if (repoUrl && credentialsId) {
-        stage("Checkout Config Repo for ${environment}") {
-            git url: repoUrl, credentialsId: credentialsId
-        }
-    } else {
-        echo "Using previously checked-out configuration repo for ${environment}."
+    stage("Checkout Config Repo for ${environment}") {
+        git url: repoUrl, credentialsId: credentialsId
     }
 
     stage("Apply ConfigMap and Secret for ${environment}") {
-        // Applying ConfigMap
-        sh "kubectl apply -f ${environment}/configmaps/ -n ${namespace}"
-        
-        // Applying Secrets
-        sh "kubectl apply -f ${environment}/secrets/ -n ${namespace}"
+        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: awsCredentialsId]]) {
+            // Applying ConfigMap
+            sh "kubectl apply -f ${environment}/configmaps/ -n ${namespace} --validate=false"
+            
+            // Applying Secrets
+            sh "kubectl apply -f ${environment}/secrets/ -n ${namespace} --validate=false"
+        }
     }
 }
