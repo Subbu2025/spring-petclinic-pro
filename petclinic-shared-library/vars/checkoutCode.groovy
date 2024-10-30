95af35ed5ea95c4f27ad3159435f82c1760c3f04
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-10-30 22:14:13 +0530

    Update checkoutCode.groovy

--- petclinic-shared-library/vars/checkoutCode.groovy
+++ petclinic-shared-library/vars/checkoutCode.groovy
@@ -2,7 +2,7 @@ def call(String branch = 'main') {
     stage('Checkout Code') {
         checkout([
             $class: 'GitSCM',
-            branches: [[name: main]],
+            branches: [[name: branch]],
             userRemoteConfigs: [[url: 'https://github.com/Subbu2025/spring-petclinic-pro.git']]
         ])
     }
