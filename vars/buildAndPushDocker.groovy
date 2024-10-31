f2d7b8b15a69c6352bfa9f7faeec5aaa3e2c4d11
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-10-31 16:41:11 +0530

    Update buildAndPushDocker.groovy

--- vars/buildAndPushDocker.groovy
+++ vars/buildAndPushDocker.groovy
@@ -1,38 +1,29 @@
 // vars/buildAndPushDocker.groovy
-
 def call(Map params) {
-    // Ensure required parameters are passed
     assert params.containsKey('repoUrl') : "Missing required parameter: repoUrl"
     assert params.containsKey('awsCredentialsId') : "Missing required parameter: awsCredentialsId"
     assert params.containsKey('dockerImageName') : "Missing required parameter: dockerImageName"
-    assert params.containsKey('awsRegion') : "Missing required parameter: awsRegion"
+    assert params.containsKey('shortCommitSha') : "Missing required parameter: shortCommitSha"
 
-    // Retrieve parameters and Jenkins environment variables
     String repoUrl = params.repoUrl
     String awsCredentialsId = params.awsCredentialsId
     String dockerImageName = params.dockerImageName
-    String awsRegion = params.awsRegion
-    String branchName = env.GIT_BRANCH?.replaceAll('/', '-') ?: 'dev'  // Default to 'dev' if not found
-    String shortCommitSha = env.GIT_COMMIT?.take(7) ?: 'latest'  // Default to 'latest' if not found
+    String prId = params.prId
+    String shortCommitSha = params.shortCommitSha
     String buildNumber = env.BUILD_NUMBER ?: '0'
 
-    // Generate the ECR-compatible image tag
-    String imageTag = "${branchName}-${shortCommitSha}-build-${buildNumber}"
+    // Updated ECR image tag to include the PR ID, commit SHA, and build number
+    String imageTag = "${prId}-${shortCommitSha}-build-${buildNumber}"
     String ecrImageTag = "${repoUrl}:${imageTag}"
 
     // Build Docker Image
-        sh "docker build -t ${ecrImageTag} ."
-    
+    sh "docker build -t ${ecrImageTag} ."
 
     // Push Docker Image to ECR
-        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: awsCredentialsId]]) {
-            // Log in to ECR using the specified region
-            sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${repoUrl}"
-            
-            // Push the image to ECR
-            sh "docker push ${ecrImageTag}"
-        }
-    
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: awsCredentialsId]]) {
+        sh "aws ecr get-login-password --region ${params.awsRegion} | docker login --username AWS --password-stdin ${repoUrl}"
+        sh "docker push ${ecrImageTag}"
+    }
 
     echo "Successfully built and pushed Docker image: ${ecrImageTag}"
 }
