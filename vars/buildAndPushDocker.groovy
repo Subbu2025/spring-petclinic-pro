90a52b463ff829b5e8609aac777c26c19f42d487
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-10-31 16:02:41 +0530

    Update buildAndPushDocker.groovy

--- vars/buildAndPushDocker.groovy
+++ vars/buildAndPushDocker.groovy
@@ -5,13 +5,15 @@ def call(Map params) {
     assert params.containsKey('repoUrl') : "Missing required parameter: repoUrl"
     assert params.containsKey('awsCredentialsId') : "Missing required parameter: awsCredentialsId"
     assert params.containsKey('dockerImageName') : "Missing required parameter: dockerImageName"
+    assert params.containsKey('awsRegion') : "Missing required parameter: awsRegion"
 
     // Retrieve parameters and Jenkins environment variables
     String repoUrl = params.repoUrl
     String awsCredentialsId = params.awsCredentialsId
     String dockerImageName = params.dockerImageName
-    String branchName = env.GIT_BRANCH?.replaceAll('/', '-') ?: 'unknown'
-    String shortCommitSha = env.GIT_COMMIT?.take(7) ?: 'unknown'
+    String awsRegion = params.awsRegion
+    String branchName = env.GIT_BRANCH?.replaceAll('/', '-') ?: 'dev'  // Default to 'dev' if not found
+    String shortCommitSha = env.GIT_COMMIT?.take(7) ?: 'latest'  // Default to 'latest' if not found
     String buildNumber = env.BUILD_NUMBER ?: '0'
 
     // Generate the ECR-compatible image tag
@@ -23,11 +25,11 @@ def call(Map params) {
         sh "docker build -t ${ecrImageTag} ."
     }
 
-    // Tag and Push Docker Image to ECR
+    // Push Docker Image to ECR
     stage("Push Docker Image to ECR") {
         withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: awsCredentialsId]]) {
-            // Log in to ECR
-            sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${repoUrl}"
+            // Log in to ECR using the specified region
+            sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${repoUrl}"
             
             // Push the image to ECR
             sh "docker push ${ecrImageTag}"
