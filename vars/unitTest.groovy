33488734af39ef68637960d20431dcdfe996148b
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-11-01 11:18:21 +0530

    Delete vars directory

--- vars/unitTest.groovy
+++
@@ -1,7 +0,0 @@
-def call(Map params = [:]) {
-    String testCommand = params.get('testCommand', './mvnw test')
-    String stageName = params.get('stageName', 'Unit Testing')
-
-    echo "Running tests with command: ${testCommand}"
-    sh testCommand
-}
